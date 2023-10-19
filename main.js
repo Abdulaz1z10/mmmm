dc11fc9554d08226f3dcf4ffed50d72b1ca86c95
Your Name <you@example.com>
2023-10-19 11:32:12 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+// let math = prompt(`ishorani tanlang`)
+// let number1 = +prompt(`1-sonni kiriting`)
+// let number2 = +prompt(`2-sonni kiriting`)
+// function vektorKalla(a,b) {
+//     let result = null
+//     if(math == '+'){
+//         result = a + b
+//     }else if(math == '-'){
+//         result = a - b
+//     }else if(math == '*'){
+//         result = a * b
+//     }else if(math == '/'){
+//         result = a / b
+//     }else{
+//         result = 'null'
+//     }
+//     return result
+// }
+// console.log(vektorKalla(number1,number2))
+
+
+
+
+let arr = [1,2,3,4,5]
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 == 0){
+            console.log(arr[i])
+        }else if(arr[i] % 2 !== 0){
+            console.log(arr[i])
+        }
+    }
